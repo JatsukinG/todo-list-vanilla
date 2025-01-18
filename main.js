1d704f6cb4ad28b83bd3b1131acba141e9b412fa
JatsukinG <marketplacepremiumdev@gmail.com>
2025-01-17 23:19:56 -0500

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-let a = "Hi world!"
+const taskForm = document.querySelector('#task-form');
 
-console.log(a)
+let tasks = []
+
+function addTask(name) {
+    tasks = [
+        ...tasks,
+        {
+            name: name,
+            completed: false
+        }
+    ]
+
+    console.log(name)
+}
+
+function removeTask(name) {
+    tasks.splice(tasks.indexOf(name), 1)
+}
+
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const name = document.getElementById('task-input').value
+
+    addTask(name)
+})
